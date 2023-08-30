3012b5f63acd5ec29f5bceb3ca0edab94527e844
noushee123 <127479183+noushee123@users.noreply.github.com>
2023-08-30 13:02:17 +0530

    MoveallNegativeNumsToOneSideOfAnArray.cpp

---
+++ MoveallNegativeNumsToOneSideOfAnArray.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+void move(vector<int> arr){
+     sort(arr.begin(),arr.end());
+}
+vector<int> arr = {-1, 2, -3, 4, 5, 6, -7, 8, 9 };
+move(arr);
+if( e : arr)
+  cout<<e<<" ";
+    return 0;
+}
